178c9056efc36ea3244ba17788b1afb68ec2c0d3
benjamin <benjamin@idavidson.ca>
2021-03-29 13:13:28 -0400

    working script with args

--- huh_overages.py
+++ huh_overages.py
@@ -1,24 +1,22 @@
+#!/usr/bin/env python3
+
 import pandas as pd
+import argparse
+
+from pandas.io import parsers
 
 
-def fileOpen():
-    data = pd.read_csv(r"~/Desktop/huh_data/raw-data.csv")
+def fileOpen(file):
+    data = pd.read_csv(file)
     df = pd.DataFrame(
         data, columns=['Tenant UUID', 'Hour', 'Host Name', 'Host Units'])
     df = df.rename(columns={'Host Units': 'HHU'})
-    # print(df)
-    # print("Info")
-    # df.info()
 
     # Drop the Summary Rows
-    # print("Drop NaN")
     df = df.dropna()
 
-    # Covert to datea
-    # print("Convert date")
+    # Covert to date
     df['Hour'] = pd.to_datetime(df['Hour'], format='%Y-%m-%d %H:%M:%S')
-    # df.info()
-    # print(df)
 
     return df
 
@@ -30,18 +28,30 @@ def csvExport(df):
     df.to_csv("~/Desktop/huh_data/hourly_hhu_total.csv", index=False)
 
 
-hostUnitLimit = 320
+# Commmand line args parsing
+parser = argparse.ArgumentParser(
+    description="Find when host units hours were consumed"
+)
+parser.add_argument("raw_data_file", nargs=1, metavar="<raw-data-file>")
+parser.add_argument("-hu", "--hostunits", required=True,
+                    help="The account host unit limit")
+args = parser.parse_args()
 
-df = fileOpen()
+rawDataFile = args.raw_data_file[0]
+hostUnitLimit = args.hostunits
+
+print()
+print(rawDataFile)
+print(type(rawDataFile))
+print()
+
+df = fileOpen(rawDataFile)
 
 # Group by each hour
 hourlyDF = df.groupby("Hour", as_index=False)['HHU'].sum()
-# print(hourlyDF)
-
-# hourlyDF.info()
 
 # Add the excess column
-hourlyDF['Excess'] = hourlyDF['HHU'] - hostUnitLimit
+hourlyDF['Excess'] = hourlyDF['HHU'] - int(hostUnitLimit)
 
 # Remove 'negative excess'
 hourlyDF.loc[hourlyDF['Excess'] <= 0, 'Excess'] = 0
